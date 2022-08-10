df7d50693fa73dd60f97a86a2c20407e5a609738
Agnes <agsawicka@gmail.com>
2022-08-10 13:46:42 -0400

    commit tree 72adbdff6a840baab32caed70e91bec5eed9c3d2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        new_arr =[]
+        arr.each do |el|
+            if el.chars.sort == @word.chars.sort
+                new_arr << el
+            end
+            
+        end 
+        pp new_arr
+        # loop over arr and check if el in arr are the same as word
+        
+        # arr.each do |ele|
+        #     if ele.chars.sort == word.chars.sort
+        #         puts ele
+        #     end
+       
+    end
+end
+
+
+
+
+# take a word
+# compare it to 
